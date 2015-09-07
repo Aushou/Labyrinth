c05ab987173509df46bd3b2734e8ae325f369c87
Tim Huang <huangc8@rpi.edu>
2015-09-07 16:59:39 -0400

    TextBox script

---
+++ Assets/Scripts/TextBox.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextBox : MonoBehaviour {
+
+	public GameObject textPanel;
+	public Text textBox;
+	float textDelay = 2;
+
+	void Start(){
+		textBox.text = "";
+		textPanel.SetActive(false);
+		textBox.enabled = false;
+	}
+
+	// show the given message
+	public void showMessage(string line){
+		StartCoroutine (show (line));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {		
+			showMessage("Hello Dude");
+		}
+	}
+
+	IEnumerator show(string line){
+		textBox.text = line;
+		textPanel.SetActive(true);
+		textBox.enabled = true;
+		yield return new WaitForSeconds (textDelay);
+		textPanel.SetActive(false);
+		textBox.enabled = false;
+		textBox.text = "";
+	}
+}
