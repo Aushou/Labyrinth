2594959775d6e4c1da36f1ab75ce25c4654a81b2
Tim Huang <huangc8@rpi.edu>
2015-09-07 15:40:59 -0400

    Player Class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int row; // current row position
+	public int col; // current col position
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// move the player: 0-right, 1-left, 2-up, 3-down
+	public void MovePlayer(int direction){
+		if (checkValid (direction)) {
+			// move the player
+		}
+	}
+
+	// check if movement is valid
+	bool checkValid(int direction){
+		if (true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
