58960629b9a9387bcb1c0a9ff37004eca68b0aa8
Tim Huang <huangc8@rpi.edu>
2015-09-08 23:35:09 -0400

    Player Update with Movement Ghost

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,34 +1,164 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
-	public int playerPos_x; // current x position
-	public int playerPos_y; // current y position
-	
-	// Update is called once per frame
-	void Update () {
-		// draw the player sprite
+	private int player_num = 4;
 
+	public List<GameObject> players;
+	public GameObject player_1;
+	public GameObject player_2;
+	public GameObject player_3;
+	public GameObject player_4;
+	public GameObject player_ghost;
+
+	private int curPlayer = 1;
+	private GameLogic _gL;
+	private Camera mainCam;
+	
+	public bool MovementMode = true;
+
+	// initiation
+	void Start(){
+		_gL = GetComponent<GameLogic> ();
+		mainCam = GetComponentInChildren<Camera> ();
+
+		// randomly assign player start
+		// initiate list
+		players = new List<GameObject> ();
+		players.Add (player_1);
+		players.Add (player_2);
+		players.Add (player_3);
+		players.Add (player_4);
+		player_ghost.SetActive (false);
+
+		// randomly assign positions
+		int[] givenPos = new int[player_num];
+		for (int i = 0; i < player_num; i++) {
+			givenPos[i] = -1;
+		}
+		for (int i = 0; i < player_num; i++) {
+			int num = -1;
+			bool invalid;
+
+			do{
+				invalid = false;
+				num = Random.Range (0, 8);
+				for(int j = 0; j < player_num; j++){
+					if(givenPos[j] == num){
+						invalid = true;
+						break;
+					}
+				}
+			}while(invalid);
+
+			givenPos[i] = num;
+		}
+		int k = 0;
+		foreach(GameObject player in players){
+
+			switch(givenPos[k]){
+			
+			case 0: 
+				player.transform.position = new Vector3(15, 17, 0);
+				break;
+			case 1:
+				player.transform.position = new Vector3(13, 17, 0);
+				break;
+			case 2:
+				player.transform.position = new Vector3(11, 17, 0);
+				break;
+			case 3:
+				player.transform.position = new Vector3(11, 16, 0);
+				break;
+			case 4:
+				player.transform.position = new Vector3(11, 14, 0);
+				break;
+			case 5:
+				player.transform.position = new Vector3(11, 13, 0);
+				break;
+			case 6:
+				player.transform.position = new Vector3(13, 13, 0);
+				break;
+			case 7:
+				player.transform.position = new Vector3(15, 13, 0);
+				break;
+			}
+			k++;
+		}
 	}
 
-	// move the player
-	public void MovePlayer(int x, int y){
-		if (checkValid (x, y)) {
-			// move the player
+	// place a ghost at where the mouse is
+	void PlayerGhost() {
+		TrackMouse ();
+
+		bool overlap = false;
+		bool adjacent = false;
+		Vector2 ghost_pos = player_ghost.transform.position;
+
+		if (checkValid()) {
+			player_ghost.GetComponent<SpriteRenderer> ().color = Color.green;
+		}else{
+			player_ghost.GetComponent<SpriteRenderer> ().color = Color.red;
 		}
+	}
+
+	// track player's mouse
+	void TrackMouse(){
+		Vector3 mousePos = mainCam.ScreenToWorldPoint (Input.mousePosition);
+		Vector2 mousePos2D;
+		
+		mousePos.z = 0;
+		mousePos.x = Mathf.RoundToInt (mousePos.x);
+		mousePos.y = Mathf.RoundToInt (mousePos.y);
+		mousePos2D.x = mousePos.x;
+		mousePos2D.y = mousePos.y;
+		
+		player_ghost.transform.position = mousePos2D;
 	}
 
 	// check if movement is valid
-	bool checkValid(int x, int y){
+	bool checkValid(){
 
-		// check if out of bound
-		if (x > 30 || x < 0) {
-			return false;
-		} else if (y > 30 || y < 0) {
-			return false;
+		// north
+		Vector3 north = players[curPlayer-1].transform.position;
+		north.y++;
+		if (player_ghost.transform.position == north) {
+			return true;
 		}
+		// south
+		Vector3 south = players[curPlayer-1].transform.position;
+		south.y--;
+		if (player_ghost.transform.position == south) {
+			return true;
+		}
+		// east
+		Vector3 east = players[curPlayer-1].transform.position;
+		east.x--;
+		if (player_ghost.transform.position == east) {
+			return true;
+		}
+		// west
+		Vector3 west = players[curPlayer-1].transform.position;
+		west.x++;
+		if (player_ghost.transform.position == west) {
+			return true;
+		}
+		return false;
+	}
 
-		return true;
+	// actually move the player
+	public void MovePlayer(){
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (MovementMode) {
+			//curPlayer = _gL.curPlayer;
+			player_ghost.SetActive(true);
+			PlayerGhost();
+		}
 	}
 }
